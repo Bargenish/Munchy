48a7c53ac45101950d2e9decde63ccd57118c9e7
Jonathan Berg <jon.berg@cs.colman.ac.il>
2018-08-05 22:34:59 +0300

    Added latlong service

--- src/app/services/cities.service.ts
+++ src/app/services/cities.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-import {Observer} from 'rxjs/Observer';
-import { observable } from '../../../node_modules/rxjs';
+import { Observer } from 'rxjs/Observer';
 
 const httpOptions = {
   headers: new HttpHeaders({
