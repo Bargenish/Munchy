bb044dc6275654520e460f64f6dae8d83f9fe91e
Bar Genish <genishbar@gmail.com>
2018-08-07 11:44:17 +0300

    kendo grid implementation

--- src/app/pages/sellers/sellers.component.ts
+++ src/app/pages/sellers/sellers.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SortDescriptor, orderBy } from '@progress/kendo-data-query';
+import { GridDataResult } from '@progress/kendo-angular-grid';
 
 @Component({
   selector: 'app-sellers',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sellers.component.scss']
 })
 export class SellersComponent implements OnInit {
-
+  public sort: SortDescriptor[] ;
+  public gridViewData: GridDataResult;
+  public sellersList: any[];
   constructor() { }
 
   ngOnInit() {
+    this.gridViewData = {
+      data: this.sellersList,
+      total: this.sellersList.length
+  };
   }
-
+  public sortChange(sort: SortDescriptor[]): void {
+    this.sort = sort;
+    this.loadSellers();
+}
+private loadSellers(): void {
+  this.gridViewData = {
+      data: orderBy(this.sellersList, this.sort),
+      total: this.sellersList.length
+  };
+}
 }
