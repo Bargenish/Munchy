8289920b1f2fe7feea5c3603f39fc30320ef8360
Bar Genish <genishbar@gmail.com>
2018-08-08 15:41:20 +0300

    bug fixing

--- src/app/pages/sellers/crud_dialogs/edit/edit.component.ts
+++ src/app/pages/sellers/crud_dialogs/edit/edit.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit.component.scss']
 })
 export class EditComponent implements OnInit {
+  public opened: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public close(status) {
+    console.log(`Dialog result: ${status}`);
+    this.opened = false;
+  }
+
+  public open() {
+    this.opened = true;
   }
 
 }
